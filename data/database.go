0205998e4cab164f73aea1e0af74182b86cd2c40
3DX900 <3DX900@gmail.com>
2020-08-20 19:49:05 +0200

    debug - trying to remove import cycle

--- data/database.go
+++ data/database.go
@@ -3,13 +3,13 @@ package data
 import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
-	"github.com/netflixaddicts/Go-API/structs"
 	"log"
+	"time"
 )
 
 func New() *Data {
 	return &Data{
-		Sheets: map[string]*structs.Sheet{},
+		Sheets: map[string]*Sheet{},
 	}
 }
 
@@ -21,10 +21,10 @@ func (d *Data) Connect(addr string, user string, password string) {
 	}
 	log.Printf("Database connected!")
 	d.db = db
-	db.AutoMigrate(&structs.Sheet{})
+	db.AutoMigrate(&Sheet{})
 
 	log.Print("Loading Sheets...")
-	var sheets []*structs.Sheet
+	var sheets []*Sheet
 	db.Find(&sheets)
 	for _, sheet := range sheets {
 		d.Sheets[sheet.Name] = sheet
@@ -32,18 +32,18 @@ func (d *Data) Connect(addr string, user string, password string) {
 	log.Printf("%d Sheets has been loaded!", len(d.Sheets))
 }
 
-func (d *Data) getSheets() ([]*structs.Sheet, error) {
-	var sheets []*structs.Sheet
+func (d *Data) getSheets() ([]*Sheet, error) {
+	var sheets []*Sheet
 	d.db.Find(&sheets)
 
 	return sheets, nil
 }
 
-func (d *Data) getSheet(id string) (structs.Sheet, error) {
-	return structs.Sheet{}, nil
+func (d *Data) getSheet(id string) (Sheet, error) {
+	return Sheet{}, nil
 }
 
-func (d *Data) addSheet(s structs.Sheet) error {
+func (d *Data) addSheet(s Sheet) error {
 	return nil
 }
 
@@ -55,6 +55,18 @@ func (d *Data) Close() {
 }
 
 type Data struct {
-	Sheets map[string]*structs.Sheet
+	Sheets map[string]*Sheet
 	db     *gorm.DB
+}
+
+type Sheet struct {
+	ID        int       `json:"id" gorm:"Type:INT(11); NOT NULL; AUTO_INCREMENT; PRIMARY_KEY"`
+	Name      string    `json:"name" gorm:"Type:VARCHAR(64); NOT NULL;"`
+	Title     string    `json:"title" gorm:"Type:VARCHAR(64); NOT NULL;"`
+	Image     string    `json:"image" gorm:"Type:VARCHAR(255)"`
+	Note      int       `json:"note" gorm:"Type:INT(11); NOT NULL; DEFAULT:'-1'"`
+	Styles    []uint8   `json:"styles" gorm:"Type:ENUM('Humour', 'Effrayant', 'Comédie');"`
+	Synopsis  string    `json:"synopsis" gorm:"Type:LONGTEXT; NOT NULL;"`
+	CreatedAt time.Time `json:"created_at" gorm:"Type:DATETIME; NOT NULL; DEFAULT:CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"Type:DATETIME; NOT NULL; DEFAULT:CURRENT_TIMESTAMP"`
 }
