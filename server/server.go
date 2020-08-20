6c8683d977f3ccb0abf68003ee059f8f2dec9a5d
3DX900 <3DX900@gmail.com>
2020-08-20 19:46:19 +0200

    debug - trying to remove import cycle

--- server/server.go
+++ server/server.go
@@ -1,13 +1,16 @@
 package server
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"github.com/netflixaddicts/Go-API/data"
-	"github.com/netflixaddicts/Go-API/router"
+	"github.com/netflixaddicts/Go-API/structs"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 type Server struct {
@@ -39,12 +42,67 @@ func (s *Server) Run(addr string) {
 }
 
 func (s *Server) handleRoutes(api *mux.Router) {
-	api.HandleFunc("/sheets", s.handleRequest(router.GetSheets)).Methods("GET")
+	api.HandleFunc("/sheets", s.handleRequest(GetSheets)).Methods("GET")
 
-	api.HandleFunc("/sheet", s.handleRequest(router.PostSheet)).Methods("POST")
-	api.HandleFunc("/sheet/{name}", s.handleRequest(router.GetSheet)).Methods("GET")
-	api.HandleFunc("/sheet/{name}", s.handleRequest(router.PatchSheet)).Methods("PATCH")
-	api.HandleFunc("/sheet/{name}", s.handleRequest(router.DeleteSheet)).Methods("DELETE")
+	api.HandleFunc("/sheet", s.handleRequest(PostSheet)).Methods("POST")
+	api.HandleFunc("/sheet/{name}", s.handleRequest(GetSheet)).Methods("GET")
+	api.HandleFunc("/sheet/{name}", s.handleRequest(PatchSheet)).Methods("PATCH")
+	api.HandleFunc("/sheet/{name}", s.handleRequest(DeleteSheet)).Methods("DELETE")
+}
+
+func GetSheets(d *data.Data, w http.ResponseWriter, r *http.Request) []byte {
+	return sendJson(w, http.StatusOK, d.Sheets)
+}
+
+func PostSheet(d *data.Data, w http.ResponseWriter, r *http.Request) []byte {
+	sheet := &structs.Sheet{}
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(sheet); err != nil {
+		return sendError(w, http.StatusBadRequest, err.Error())
+	}
+	defer r.Body.Close()
+
+	if d.Sheets[sheet.Name] != nil {
+		return sendError(w, http.StatusBadRequest, fmt.Sprintf("A sheet named '%s' already exists in database.", sheet.Name))
+	}
+	sheet.ID = len(d.Sheets) + 1
+	sheet.CreatedAt = time.Now()
+	sheet.UpdatedAt = time.Now()
+
+	d.Sheets[sheet.Name] = sheet
+	// TODO: Make DB insert query
+
+	return sendJson(w, http.StatusCreated, nil)
+}
+
+func GetSheet(d *data.Data, w http.ResponseWriter, r *http.Request) []byte {
+	vars := mux.Vars(r)
+	name := vars["name"]
+
+	sheet, ok := d.Sheets[name]
+	if !ok {
+		return sendError(w, http.StatusNotFound, fmt.Sprintf("No sheet were found with name '%s'", name))
+	}
+	return sendJson(w, http.StatusOK, sheet)
+}
+
+func PatchSheet(d *data.Data, w http.ResponseWriter, r *http.Request) []byte {
+	return sendJson(w, http.StatusOK, nil)
+}
+
+func DeleteSheet(d *data.Data, w http.ResponseWriter, r *http.Request) []byte {
+	vars := mux.Vars(r)
+	name := vars["name"]
+
+	_, ok := d.Sheets[name]
+	if !ok {
+		return sendError(w, http.StatusNotFound, fmt.Sprintf("No sheet were found with name '%s'", name))
+	}
+	delete(d.Sheets, name)
+	// TODO: Make DB remove query
+
+	return sendJson(w, http.StatusOK, nil)
 }
 
 type RequestHandler func(d *data.Data, w http.ResponseWriter, r *http.Request) []byte
@@ -55,4 +113,22 @@ func (s *Server) handleRequest(handler RequestHandler) http.HandlerFunc {
 		res := handler(s.Data, w, r)
 		log.Printf("[%s - %s - %s] Request handled, responded with '%s'", r.Method, r.RequestURI, r.RemoteAddr, res)
 	}
+}
+
+func sendError(w http.ResponseWriter, code int, message string) []byte {
+	return sendJson(w, code, map[string]string{"error": message})
+}
+
+func sendJson(w http.ResponseWriter, code int, payload interface{}) []byte {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	if payload != nil {
+		_, _ = w.Write(response)
+	}
+	return response
 }
